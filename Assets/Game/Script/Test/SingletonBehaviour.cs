255c4a0b691c01055db065d6ba92ca531578b8c9
sakamotosyo <syo10150000@gmail.com>
2022-06-08 06:03:10 +0900

    シングルトンの作成

--- Assets/Game/Script/Test/SingletonBehaviour.cs
+++ Assets/Game/Script/Test/SingletonBehaviour.cs
@@ -2,17 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SingletonBehaviour : MonoBehaviour
+//メモ
+//シングルトンはグローバルなアクセスポイントを提供するもの
+//インスペクターで値をいじらなくていいものでシングルトンを作る場合はMonoBehaviorを継承しなくてもいい
+//Monobehaviourを継承する必要がないならstatucクラスでもシングルトンとあまり変わらない
+//->それによるメリット:SingletonBehaviorクラスとの結合が回避できてnullになることがなくエラーが起こっても対処しやすい
+//->デメリット:変数の値を手動で初期化しなければならない。Textureなどのリソースへの参照を持つ場合Nullを入れないと解放されない
+
+public class SingletonBehaviour<T> : MonoBehaviour where T : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private T _instance;
+
+    //カプセル化
+    public T Instance => _instance;
+                     
+    //チーム制作の時はこう書いたほうがいい
+    //public static T Instance 
+    //{
+    //    get 
+    //    {
+    //        if (_instance == null) 
+    //        {
+    //            Type t = typeof(T);
+    //            Instance = (T)FindObjectOfType<t>;
+
+    //            if(_instance == null) 
+    //            {
+    //                Debug.LogError($"{t}をアタッチしているGameObjectがありません");
+    //            }
+    //        }
+
+    //        return _instance;
+    //    }
+    //}
+
+    protected virtual void Awake() 
     {
-        
+        OnAwake();
+        ChackIns();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    //継承先でAwakeが必要な場合はこれを呼ぶ
+    protected virtual void OnAwake() { }
+
+    protected void ChackIns() 
     {
-        
+        if (_instance == null)
+        {
+            //キャストの意味を理解する。T型にキャストしている
+            _instance = this as T;
+        }
+        else if (Instance == this)
+        {
+            return;
+        }
+        else if (Instance != this) 
+        {
+            //すでにあった時は何もせずに消える
+        }
     }
+
 }
